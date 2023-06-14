483a33ee2676f3a56c4086b25fb51cabbfb359b4
Lakshay <135457408+Lakshay-bit-png@users.noreply.github.com>
2023-06-14 21:40:45 +0530

    Update stylin.js

--- stylin.js
+++ stylin.js
@@ -1,3 +1,6 @@
 function changecolor(x){ document.getElementById(x).style.color='Red';}
 function changecolor2(x){document.getElementById(x).style.color='Black';}
 
+function doctor(){
+    location.assign("doctorxconnect.html")
+}

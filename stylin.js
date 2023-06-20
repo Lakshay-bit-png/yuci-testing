33f8287c50625dfac086faba432744d4ac3cd80e
Lakshay <135457408+Lakshay-bit-png@users.noreply.github.com>
2023-06-20 22:03:34 +0530

    Update stylin.js

--- stylin.js
+++ stylin.js
@@ -1,5 +1,9 @@
-function changecolor(x){ document.getElementById(x).style.color='Red';}
-function changecolor2(x){document.getElementById(x).style.color='Black';}
+function changecolor(x){ document.getElementById(x).style.color='rgb(164, 24, 78)';
+                       
+                        }
+function changecolor2(x){document.getElementById(x).style.color='Black';
+}
+
 
 function doctor(){
     location.assign("doctorxconnect")
